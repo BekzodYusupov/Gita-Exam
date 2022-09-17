0c7dc96e67a6444669bccf361f60720fb91695ba
Gilfoyle-dev <bek.droid.dev@gmail.com>
2022-09-17 14:20:44 +0500

    Hey

--- app/src/main/java/uz/gita/gitaexam/MainActivity.kt
+++ app/src/main/java/uz/gita/gitaexam/MainActivity.kt
@@ -2,10 +2,13 @@ package uz.gita.gitaexam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Toast
     }
 }
